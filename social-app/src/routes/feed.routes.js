fd8606e057d1752c22706c6b2efab94d73aab84e
Pranjali <pranjalidas2201@gmail.com>
2026-05-16 05:19:54 -0700

    middleware - auth - feed

--- social-app/src/routes/feed.routes.js
+++ social-app/src/routes/feed.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getHomeFeed, getExploreFeed } = require('../controllers/feed.controller');
+const auth = require('../middlewares/auth');
 
-router.get('/home', getHomeFeed);
+
+router.get('/home', auth, getHomeFeed);
 router.get('/explore', getExploreFeed);
 
 
