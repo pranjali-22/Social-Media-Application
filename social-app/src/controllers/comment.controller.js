c79a160413aeb0f287d97b91fe21020cdeb35c9b
Pranjali <pranjalidas2201@gmail.com>
2026-05-16 02:41:32 -0700

    addComment - error fix

--- social-app/src/controllers/comment.controller.js
+++ social-app/src/controllers/comment.controller.js
@@ -1,22 +1,25 @@
-const Comment = require('./models/Comment');
-const Post = require('../models/Post')
+const Comment = require('../models/Comment');
+const Post = require('../models/Post');
 
-exports.addCommnet = async(req,res) => {
-    try{
-        const {content} = req.body;
-        const{postId} = req.params;
+exports.addComment = async (req, res) => {
+    try {
+        const { content } = req.body;
+        const { postId } = req.params;
+
         const post = await Post.findById(postId);
+        if (!post) return res.status(404).json({ success: false, error: { message: 'Post not found' } });
 
-        if (!post) return res.status(404).json({success:false,error:'Post not found'})
         const comment = await Comment.create({
-            post : postId,
+            post: postId,
             user: req.user.sub,
             content
         });
 
-        await Post.findByIdANdUpdate(postId, $inc:{cmmentCount: 1})
-        res.status(201).json({success:true, data:comment});
-    } catch(err){
-        res.status(500).json({success: false, error : {message: err.message}})
+        await Post.findByIdAndUpdate(postId, { $inc: { commentCount: 1 } });
+
+        res.status(201).json({ success: true, data: comment });
+    } catch (err) {
+        res.status(500).json({ success: false, error: { message: err.message } });
     }
-}
+};
+
--- social-app/src/routes/comment.routes.js
+++ social-app/src/routes/comment.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {addComment} = require('../controllers/comment.controller')
 
 router.post('./:postId',addComment);
+router.get('./:postId', getComments);
 
 
 module.exports = router;
